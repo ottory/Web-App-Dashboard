21afabca18e2839470ea49efee083ba65b9cfd4b
ottory <56276237+ottory@users.noreply.github.com>
2020-10-01 20:27:19 -0700

    added svg and css styles
    need to lay out the media qs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const alertBanner = document.getElementById("alert");
 const trafficCanvas = document.querySelector('#traffic-chart');
 
+const dailyCanvas = document.querySelector('#daily-chart');
+const mobileCanvas = document.querySelector('#mobile-chart');
+
+
+const user = document.querySelector('#userField');
+const message = document.querySelector('#messageField');
+const send = document.querySelector('#send');
+
+
+
 /// create the html for the banner 
 
   alertBanner.innerHTML = 
@@ -28,7 +38,7 @@ let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
   datasets: [{
     data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-    background: 'rgba(116, 119, 191, .3)',
+    backgroundColor: '#7477bf',
     borderWidth: 1,
   }]
 };
@@ -57,3 +67,84 @@ options: trafficOptions
 });
 
 
+//////// Daily Chart 
+
+let dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets:[{
+      label: '# of Hits',
+      data: [50, 100, 150, 200, 250, 300, 350],
+      backgroundColor: '#7477bf',
+      borderWidth: 1,
+    }]
+};
+
+let dailyOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true
+      }
+    }]
+  },
+  legend : {
+    display: false
+  }
+  };
+
+  let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+    });
+
+//////// Mobile Chart 
+
+let mobileData = {
+  labels: ['Desktop', 'Tablet', 'Phones'],
+  datasets: [{
+    label: '# of Users',
+    data: [3000, 60, 575],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477bf',
+      '#78cf82',
+      '#51b6c8'
+    ]
+    
+    
+  }]
+};
+
+let mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 20,
+      fontStyles: 'bold'
+    }
+  }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+
+
+});
+
+////// Message fields
+
+send.addEventListener('click', () =>  {
+  //ensure user and message fields are filled out 
+    if (user.value === "" && message.value === "") {
+      alert ("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+         alert ("Please fill out user field before sending");
+      } else if (message.value === "") {
+        alert ("Please fill out user field before sending");
+      } else {
+        alert (`Message successfully sent to: ${user.value}`)
+      }
+});
